eb2c7ecdd3817b760a88efac7ed5e04803edc554
knavinkumarkannan <knavinkumar47@gmail.com>
2026-09-02 14:11:50 +0530

    Unix-pro 9-1U25IT062

--- mv_operations.sh
+++ mv_operations.sh
@@ -1,8 +1,19 @@
-#!/bin/bash
-
 case "$1" in
-    move) mv file1.txt documents/ ;;
-    rename) mv file1.txt file2.txt ;;
-    multiple) mv file1.txt file2.txt documents/ ;;
-    directory) mv move_me_dir documents/ ;;
+
+move)
+    mv file1.txt documents/
+    ;;
+
+rename)
+    mv file1.txt file2.txt
+    ;;
+
+multiple)
+    mv file1.txt file2.txt documents/
+    ;;
+
+directory)
+    mv source_dir documents/
+    ;;
+
 esac
